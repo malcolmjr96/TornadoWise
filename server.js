da1a878bf0cd0d89258d769d1fff08e4ffaac09e
Malcolm Melbourne <malcolm_melbourne@outlook.com>
2025-04-23 08:16:43 -0400

    Add QGIS section and update dependencies; include new images and styles

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
-let express = require('express');
+let express = require("express");
+let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 let app = express();
 const port = 8080;
 
-app.use(express.static('public'));
+app.use(express.json());
+app.use(express.static("public"));
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + 'public/index.html');
+app.get("/", function (req, res) {
+    console.log("Connection received.");
+    res.sendFile("./public/index.html");
 });
 
-app.listen(port, function(req, res){
+app.post("/searchCity", function (req, res) {
+    let xhttp = new XMLHttpRequest();
+    let url = "https://geogratis.gc.ca/services/geoname/en/geonames.json?q=";
+    let receivedData = {
+        userInput: req.body.userInput,
+    };
+
+    console.log(receivedData);
+    xhttp.open("GET", url + receivedData.userInput, true);
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    xhttp.onload = function () {
+        if (!xhttp.status === "200") {
+            console.log(xhttp.responseText);
+            // console.log(xhttp.responseText);
+        } else {
+            res.send(xhttp.responseText);
+            // console.log(xhttp.responseText);
+        }
+    };
+    xhttp.send();
+});
+
+app.listen(port, function (req, res) {
     console.log(`Listening on port ${port}.`);
 });
